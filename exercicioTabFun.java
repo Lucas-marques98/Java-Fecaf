4173bcd71ceec94a9e30e216589c37c2b15fd176
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-09-12 13:41:58 -0300

    arrumando ex

--- exercicioTabFun.java
+++ exercicioTabFun.java
@@ -4,8 +4,8 @@ public class exercicioTabFun {
 
 	public static class Exercicio_Tabuada {
 		public static void main(String[] args) {
-			String tabuada = null;
-			int valor_inicial;
+				String tabuada = null;
+				int valor_inicial;
 			int valor_final;
 			int cont = 0;
 			int resultado;
@@ -18,7 +18,7 @@ public class exercicioTabFun {
 				System.out.println("Favor digitar um valor inicial a ser calculado:");
 				valor_inicial = leitor.nextInt();
 
-				if (valor_inicial == 2 && valor_inicial <= 100) {
+				if (valor_inicial >= 2 && valor_inicial <= 100) {
 					while (cont <= valor_inicial) {
 						resultado = valor_inicial * cont;
 						System.out.println(valor_inicial + "x" + cont + "=" + resultado);
@@ -26,6 +26,7 @@ public class exercicioTabFun {
 					}
 				} else {
 					System.out.println("informação inválida! Digite de 2 a 100");
+					repeat();
 				}
 
 				System.out.println("Favor digitar um valor final a ser calculado:");
@@ -39,6 +40,7 @@ public class exercicioTabFun {
 					}
 				} else {
 					System.out.println("informação inválida! Digite de 2 a 100");
+					repeat();
 					;
 				}
 
@@ -52,49 +54,52 @@ public class exercicioTabFun {
 		}
 	}
 
-	public static void repeat() {
-		String tabuada = null;
-		int valor_inicial;
-		int valor_final;
-		int cont = 0;
-		int resultado;
-		System.out.println("Olá, bem vindo ao meu sistema de tabuada \n"
-				+ "Digite Tabuada para prosseguir! ");
-		Scanner leitor = new Scanner(System.in);
-		tabuada = leitor.nextLine().toUpperCase();
+		public static void repeat() {
+			String tabuada = null;
+			String erro = null;
+			int valor_inicial;
+			int valor_final;
+			int cont = 0;
+			int resultado;
+			System.out.println("Olá, bem vindo ao meu sistema de tabuada \n"
+					+ "Digite Tabuada para prosseguir! ");
+			Scanner leitor = new Scanner(System.in);
+			tabuada = leitor.nextLine().toUpperCase();
 
-		if (tabuada.equals("TABUADA")) {
-			System.out.println("Favor digitar um valor inicial a ser calculado:");
-			valor_inicial = leitor.nextInt();
+			if (tabuada.equals("TABUADA")) {
+				System.out.println("Favor digitar um valor inicial a ser calculado:");
+				valor_inicial = leitor.nextInt();
+				erro = leitor.nextLine();
 
-			if (valor_inicial >= 2 || valor_inicial <= 100) {
-				while (cont <= valor_inicial) {
-					resultado = valor_inicial * cont;
-					System.out.println(valor_inicial + "x" + cont + "=" + resultado);
-					cont += 1;
+				if (valor_inicial >= 2 && valor_inicial <= 100) {
+					while (cont <= valor_inicial) {
+						resultado = valor_inicial * cont;
+						System.out.println(valor_inicial + "x" + cont + "=" + resultado);
+						cont += 1;
+					}
+				} else {
+					System.out.println("informação inválida! Digite de 2 a 100");
+					repeat();
 				}
-			} else {
-				System.out.println("informação inválida! Digite de 2 a 100");
-			}
 
-			System.out.println("Favor digitar um valor final a ser calculado:");
-			valor_final = leitor.nextInt();
+				System.out.println("Favor digitar um valor final a ser calculado:");
+				valor_final = leitor.nextInt();
 
-			if (valor_final >= 2 && valor_final <= 100) {
-				for (int num1 = 0; num1 <= valor_final; num1++) {
-					resultado = valor_final * num1;
-					System.out.println(valor_final + "x" + num1 + "=" + resultado);
+				if (valor_final >= 2 && valor_final <= 100) {
+					for (int num1 = 0; num1 <= valor_final; num1++) {
+						resultado = valor_final * num1;
+						System.out.println(valor_final + "x" + num1 + "=" + resultado);
 
+					}
+				} else {
+					System.out.println("informação inválida! Digite de 2 a 100");
+					repeat();
 				}
-			} else {
-				System.out.println("informação inválida! Digite de 2 a 100");
-				;
-			}
 
-		} else {
-			System.out.println("informação inválida! Digite tabuada.");
+			} else {
+				System.out.println("informação inválida! Digite tabuada.");
+
+			}
 
 		}
-
 	}
-}
